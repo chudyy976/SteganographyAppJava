4a633211b37ceb6c2af2b528a3fbbd6d81b5a6f8
chudyy976 <rafal.chudy976@gmail.com>
2020-01-25 16:51:36 +0100

    Added choosing and saving file option, without view

--- src/steg/Controller.java
+++ src/steg/Controller.java
@@ -1,10 +1,14 @@
 package steg;
 
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 
 public class Controller {
 
@@ -12,22 +16,44 @@ public class Controller {
 
     public Controller(Model model) { this.model = model; }
 
-    private ImageView originalView, modifiedView;
+    private Image image;
     private TextField fieldMessage;
+    private FileChooser fileChooser = new FileChooser();
 
-    public void injectUI(ImageView original, ImageView modified, TextField fieldMessage) {
-        this.originalView = original;
-        this.modifiedView = modified;
+    public void readImage(){
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Image Files (*.jpg, *.png)", "*.png", "*.jpg"),
+                new FileChooser.ExtensionFilter("All Files (*.*)", "*.*")
+        );
+        File file = fileChooser.showOpenDialog(null);
+        Image image = new Image(file.toURI().toString());
+        this.image = image;
+    }
+    public void injectUI(TextField fieldMessage) {
         this.fieldMessage = fieldMessage;
     }
 
-    public void onEncode() {
-        Image modified = model.encode(originalView.getImage(), fieldMessage.getText());
-        modifiedView.setImage(modified);
+    public void onEncode() throws IOException {
+        if (fieldMessage.getText() == null | fieldMessage.getText().trim().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Warning");
+            alert.setContentText("Brak wiadomosci");
+            alert.showAndWait();
+        } else {
+            fileChooser.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("Image File (*.png)", "*.png"));
+            Image modified = model.encode(image, fieldMessage.getText());
+            File file = fileChooser.showSaveDialog(null);
+            ImageIO.write(SwingFXUtils.fromFXImage((modified), null), "png", file);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Information");
+            alert.setContentText("Zakodowano pomyślnie");
+            alert.showAndWait();
+        }
     }
 
     public void onDecode() {
-        String message = model.decode(modifiedView.getImage());
+        String message = model.decode(image);
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Rozkodowana wiadomosc");
         alert.setContentText(message);
--- src/steg/View.java
+++ src/steg/View.java
@@ -1,55 +1,56 @@
 package steg;
 
-import javafx.scene.control.Alert;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.FileChooser;
+
+import javax.swing.*;
 import java.io.File;
+import java.io.IOException;
 
 public class View extends Pane {
-
     private Controller controller;
-
+    private FileChooser fileChooser = new FileChooser();
     public View(Controller controller) {
         this.controller = controller;
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("Image Files (*.jpg, *.png)", "*.png", "*.jpg"),
-                new FileChooser.ExtensionFilter("All Files (*.*)", "*.*")
-        );
-        File file = fileChooser.showOpenDialog(null);
-        ImageView originalView = new ImageView(file.toURI().toString());
-        double height = originalView.getImage().getHeight();
-        double width = originalView.getImage().getWidth();
-
-        setPrefSize(width * 2, height+50);
-
-        ImageView modifiedView = new ImageView();
-        modifiedView.setTranslateX(width);
-
-        TextField field = new TextField();
-        field.setPromptText("Wprowadz wiadomosc: ");
-        field.setTranslateY(width);
-        field.setPrefWidth(200);
-        field.setOnAction(e -> {
-            if (field.getText() == null | field.getText().trim().isEmpty()) {
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Warning");
-                alert.setContentText("Brak wiadomosci");
-                alert.showAndWait();
-            } else controller.onEncode();
-        });
+        //variables and objects
 
         Button btnDecode = new Button("DEKODUJ");
-        btnDecode.setPrefWidth(100);
-        btnDecode.setTranslateX(width*2 -100);
-        btnDecode.setTranslateY(width);
+        Button btnEncode = new Button("KODUJ");
+        Button btnRead = new Button("Wczytaj zdjecie");
+        TextField field = new TextField();
+
+        //elements aligment
+        setPrefSize(400, 200);
+        field.setPromptText("Wprowadz wiadomosc: ");
+        field.setPrefSize(200, 30);
+        field.setTranslateX(100);
+        field.setTranslateY(30);
+        btnDecode.setPrefSize(100, 70);
+        btnEncode.setPrefSize(100, 70);
+        btnDecode.setTranslateX(300);
+        btnDecode.setTranslateY(90);
+        btnEncode.setTranslateY(90);
+        btnRead.setPrefSize(100, 40);
+        btnRead.setTranslateX(150);
+        btnRead.setTranslateY(105);
+
+        //btn actions
+        btnRead.setOnAction(e -> controller.readImage());
         btnDecode.setOnAction(e -> controller.onDecode());
+        btnEncode.setOnAction(e-> {
+            try {
+                controller.onEncode();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
 
-        controller.injectUI(originalView, modifiedView, field);
+        controller.injectUI(field);
 
-        getChildren().addAll(originalView, modifiedView, field, btnDecode);
+        getChildren().addAll(field, btnDecode, btnEncode, btnRead);
     }
 }
